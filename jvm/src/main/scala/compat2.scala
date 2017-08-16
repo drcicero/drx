0c26c5740a11a956a176f8755a37e66d82d89f49
DrCicero <ohk5shae6iot@gmail.com>
2017-08-16 12:58:25 +0200

    add weakmaps

--- jvm/src/main/scala/compat2.scala
+++ jvm/src/main/scala/compat2.scala
@@ -13,4 +13,12 @@ object compat2 {
 //  type PotentialWeakHashMap[K,V] = scala.collection.mutable.HashMap[K,V]
 ////  type PotentialWeakHashMap[K,V] = new drx.debug.NoMap[K,V]
 
+  class WeakMap[Key, Value] {
+    private val map = scala.collection.mutable.WeakHashMap[Key, Value]()
+    def delete(key: Key): Unit = map.remove(key)
+    def has(key: Key): Boolean = map.contains(key)
+    def get(key: Key): Option[Value] = map.get(key)
+    def set(key: Key, value: Value): Unit = map.update(key, value)
+  }
+
 }
